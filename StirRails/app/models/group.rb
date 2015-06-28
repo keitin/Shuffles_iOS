fd028cd78cd37893aea0d21e19196e95f6e7c906
松下慶大 <atiek1121@gmail.com>
2015-06-28 20:31:34 +0900

    ユーザをシャッフルするスクリプトを作成

--- StirRails/app/models/group.rb
+++ StirRails/app/models/group.rb
@@ -9,6 +9,20 @@ class Group < ActiveRecord::Base
   #carrierWave
   mount_uploader :avatar, AvatarUploader
 
+  def shuffle_users
+    group_user_ids = self.users.map { |user| user.id }
+    shuffle_group_user_ids = group_user_ids.shuffle
+
+    pare_ids = []
+    group_user_ids.zip(shuffle_group_user_ids).each do |id, shuffle_id|
+      pare_ids << [id, shuffle_id]
+    end
+
+    pare_ids.each do |pare_id|
+      Fake.create(user_id: pare_id.first, fake_user_id: pare_id.second)
+    end
+  end
+
   def self.create_my_group(params)
     auth_token = params[:auth_token]
     user = User.by_auth_token(auth_token)
